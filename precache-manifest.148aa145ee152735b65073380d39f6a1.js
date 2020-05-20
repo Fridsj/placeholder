e781c37bb8040b93c0febdf2cfbd400e7c653c9f
Andreas <andreas.lones@gmail.com>
2020-05-20 22:08:19 +0200

    Updates

---
+++ precache-manifest.148aa145ee152735b65073380d39f6a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e6e6bc3bdc50044c08965ad60b37ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6284dfe39fad061a6c3",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "840922c2e9237eeb948e",
+    "url": "/static/js/2.8effa871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8effa871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6284dfe39fad061a6c3",
+    "url": "/static/js/main.4273df35.chunk.js"
+  },
+  {
+    "revision": "779cad542429a59c891a",
+    "url": "/static/js/runtime-main.bd48eb79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
