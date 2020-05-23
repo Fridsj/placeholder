893976efda3787cff4a1e18408860164d6614738
Andreas <andreas.lones@gmail.com>
2020-05-23 17:48:06 +0200

    Accept all paths.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Fridsj.no kommer snart.</p>
-      </header>
-    </div>
+    <Router basename="/">
+      <Switch>
+        <Route path="*">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>Fridsj.no kommer snart.</p>
+            </header>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
